c71a9947ba6fac3c4a8960011825bf33e8c4996a
Angelina Khilman <168126479+angkhilman@users.noreply.github.com>
2024-06-20 20:55:10 -0500

    Update 323.py

--- 323.py
+++ 323.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
 
 class Solution:
-    def dfs(self, v, visited, g):
-        visited[v] = 1
-        for i in g[v]:
-            if visited[i] == 0:
-                self.dfs(i, visited, g)
+    def dfs(self, node, visited, g):
+        visited[node] = 1
+        for neighbour in g[node]:
+            if visited[neighbour] == 0:
+                self.dfs(neighbour, visited, g)
 
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         graph = defaultdict(list)
